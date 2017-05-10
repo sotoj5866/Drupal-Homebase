4371b52ceb5464f0a969d36b22ac971bd8ee2755
Juan Soto <sotoj5866@gmail.com>
2017-05-10 12:04:47 -0700

    add functional tests

---
+++ modules/homebase/src/Tests/homebaseFormTest.php
@@ -0,0 +1,59 @@
+<?php
+
+  namespace Drupal\homebase\Tests;
+
+  use Drupal\simpletest\WebTestBase;
+
+  /**
+   * Tests the homebase module
+   * @group homebase
+   */
+  class HomebaseFormTest extends WebTestBase {
+    /**
+    * Modules to install.
+    *
+    * @var array
+    */
+    public static $modules = array('node', 'homebase');
+
+    /**
+    * Tests that 'homebase-form' returns a 200 OK response.
+    */
+    public function testHomebaseFormRouterURLIsAccessible() {
+      $this->drupalGet('homebase-form');
+      $this->assertResponse(200);
+    }
+
+    /**
+    * Tests that the form has a submit button to use.
+    */
+    public function testHomebaseFormSubmitButtonExists() {
+      $this->drupalGet('homebase-form');
+      $this->assertResponse(200);
+      $this->assertFieldById('edit-submit');
+    }
+
+    /**
+    * Test that the options we expect in the form are present.
+    */
+    public function testHomebaseFormFieldOptionsExist() {
+      $this->drupalGet('homebase-form');
+      $this->assertResponse(200);
+
+      // check that our select field displays on the form
+      $this->assertFieldByName('types');
+
+      // check that all of our options are available
+      $types = ['Batting', 'Catching', 'Throwing'];
+
+      foreach ($types as $type) {
+        $this->assertOption('edit-types', $type);
+      }
+
+      // check that Pizza is not an option. Sorry, pizza lovers.
+      $this->assertNoOption('edit-types', 'stealing');
+    }
+
+
+
+  }
