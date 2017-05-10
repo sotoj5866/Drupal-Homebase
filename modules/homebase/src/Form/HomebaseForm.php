439eaabbdc1860cd91f642788ab0df6c297d082c
Juan Soto <sotoj5866@gmail.com>
2017-05-10 10:30:37 -0700

    refactor HomeBaseForm

--- modules/homebase/src/Form/HomebaseForm.php
+++ modules/homebase/src/Form/HomebaseForm.php
@@ -44,13 +44,36 @@ class HomebaseForm extends FormBase {
       '#options' => $option_array,
     ];
 
+    $form['add_remove'] = array(
+      '#type' => 'radios',
+      '#title' => $this->t('Add or Remove experience'),
+      '#default_value' => 1,
+      '#options' => array(
+        1 => $this->t('Add'),
+        -1 => $this->t('Remove'),
+      ),
+    );
+
+    $form['experience'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Select amount of experience'),
+      '#options' => [
+        10 => $this->t('10'),
+        20 => $this->t('20'),
+        30 => $this->t('30'),
+        40 => $this->t('40'),
+        50 => $this->t('50'),
+        100 => $this->t('100'),
+      ],
+    ];
+
     $form['types'] = [
       '#type' => 'select',
       '#title' => $this->t('Select element'),
       '#options' => [
         'Batting' => $this->t('Batting'),
-        'Throwing' => $this->t('Throwing'),
         'Catching' => $this->t('Catching'),
+        'Throwing' => $this->t('Throwing'),
       ],
     ];
 
@@ -76,33 +99,71 @@ class HomebaseForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $player_found = $form_state->getValue('players');
-    $skill = $form_state->getValue('types');
-    // var_dump($skill);
 
 
+  public function findLevel($current_xp){
+    return floor(($current_xp + 100)/100);
+  }
+
+  public function level($player_found, $skill, $experience, $add_remove){
     $selected_player = \Drupal::entityQuery('node')->condition('type','player')->condition('title', $player_found, 'CONTAINS')->execute();
     $selected_node =  \Drupal\node\Entity\Node::loadMultiple($selected_player);
     foreach ($selected_node as $node) {
       if ($skill === 'Batting'){
-        $xp = $node->field_batting_experience->value + 10;
-        // insert algorgithm for figuring out level
+        $xp = $node->field_batting_experience->value + ($add_remove * $experience);
+        if($xp < 0){
+          $xp = 0;
+        }
+        $level = $this->findLevel($xp);
+        if($level!=$node->field_batting_level->value){
+          $node->set('field_batting_level', $level);
+          $node->save();
+        }
         $node->set('field_batting_experience', $xp);
         $node->save();
       } else if ($skill === 'Throwing'){
-        $xp = $node->field_throwing_exper->value + 10;
-        $node->set('field_throwing_exper', $xp);
+        $xp = $node->field_throwing_experience->value + ($add_remove * $experience);
+        if($xp < 0){
+          $xp = 0;
+        }
+        $level = $this->findLevel($xp);
+        if($level!=$node->field_throwing_experience->value){
+          $node->set('field_throwing_level', $level);
+          $node->save();
+        }
+        $node->set('field_throwing_experience', $xp);
         $node->save();
       } else if ($skill === 'Catching'){
-        $xp = $node->field_catching_experience->value + 10;
+        $xp = $node->field_catching_experience->value + ($add_remove * $experience);
+        if($xp < 0){
+          $xp = 0;
+        }
+        $level = $this->findLevel($xp);
+        if($level!=$node->field_catching_experience->value){
+          $node->set('field_catching_level', $level);
+          $node->save();
+        }
         $node->set('field_catching_experience', $xp);
         $node->save();
       }
 
     }
 
-    drupal_set_message("10 XP has been added to " . $player_found . "'s " . $skill . " score");
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $player_found = $form_state->getValue('players');
+    $skill = $form_state->getValue('types');
+    $experience = $form_state->getValue('experience');
+    $add_remove = $form_state->getValue('add_remove');
+
+    $this->level($player_found, $skill, $experience, $add_remove);
+
+    if($add_remove === 1){
+      drupal_set_message($experience . " XP has been added to " . $player_found . "'s " . $skill . " score");
+    }else{
+      drupal_set_message($experience . " XP has been removed from " . $player_found . "'s " . $skill . " score");
+    }
 
   }
 
